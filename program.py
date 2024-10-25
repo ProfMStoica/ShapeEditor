bb617970d6eb92f2f4c2be670df4bdd4c4472373
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2024-10-25 14:51:45 -0400

    Create interactive shapes.

--- program.py
+++ program.py
@@ -43,4 +43,35 @@ class Program:
         #calculate and print the area of the small rectangle
         smallRectArea = smallRect.calculateArea()
         print(f"The area of the small rectangle after contraction is {smallRectArea}")
+
+        #ask the user to create two circles of the different radiuses and compare them. How?
+        #ask the user for the radius of the first circle
+        radiusOne = float(input("Please enter the radius of the first circle: "))
+
+        #create the first circle
+        circleOne = Circle(radiusOne)
+
+        #ask the user for the radius of the second circle
+        radiusTwo = float(input("Please enter the radius of the second circle: "))
+
+        #create the second circle
+        circleTwo = Circle(radiusTwo)
+
+        #let the user know which ones is larger. How?
+        #calculate the areas of the two circles
+        areaOne = circleOne.calculateArea()
+        areaTwo = circleTwo.calculateArea()
+
+        #compare the areas to determine which is one is larger
+        if areaOne > areaTwo:
+            print("The first circle is larger")
+        elif areaTwo > areaOne:
+            print ("The second circle is larger")
+        else:
+            print("The circles have the same area")
+
+        #TODO: add user interactivity to allow the user to create
+        #rectangles of different dimensions and compare them
+
+        
     
